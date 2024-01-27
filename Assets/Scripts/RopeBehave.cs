45d52e812b74707d93a3dde00f3fa3cca8841c93
M-McCorkell156 <McCorkell-M@ulster.ac.uk>
2024-01-27 14:20:02 +0000

    2:15 - 27/01 - Spring Detection
    Added detection when players should spring when too far apart

--- Assets/Scripts/RopeBehave.cs
+++ Assets/Scripts/RopeBehave.cs
@@ -6,15 +6,25 @@ public class RopeBehave : MonoBehaviour
 {
     [SerializeField] public GameObject Player1;
     [SerializeField] public GameObject Player2;
+
+    private Vector3 Player1Pos;
+    private Vector3 Player2Pos;
+
+    private float DiffDis;
+
     private void Start()
     {
         
     }
     void Update()
     {
-        //if (transform.)
-        {
+        Player1Pos = Player1.transform.position;
+        Player2Pos = Player2.transform.position;
 
+        DiffDis = Vector3.Distance(Player1Pos, Player2Pos);
+        if (DiffDis >= 3)
+        {
+            Debug.Log("Too Long");
         }
     }
 }
