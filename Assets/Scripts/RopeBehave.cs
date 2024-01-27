bdbacbaec621cfbe87032978e81ce5fe9d8d31c4
M-McCorkell156 <McCorkell-M@ulster.ac.uk>
2024-01-27 14:00:58 +0000

    2 - 27/1
    working

---
+++ Assets/Scripts/RopeBehave.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeBehave : MonoBehaviour
+{
+    [SerializeField] public GameObject Player1;
+    [SerializeField] public GameObject Player2;
+    private void Start()
+    {
+        
+    }
+    void Update()
+    {
+        //if (transform.)
+        {
+
+        }
+    }
+}
